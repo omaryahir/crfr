b72241dae6078e62a202423fba77323e7eb34880
omaryahir <1206997+omaryahir@users.noreply.github.com>
2020-02-29 20:39:09 -0600

    FR002: Ignoring some additional files and configuring a local settings file

--- back/config.py
+++ back/config.py
@@ -1,14 +1,13 @@
-import os 
-from localsettings import *
+from local_settings import CRFR
 
 class BaseConfig(object):
-    SECRET_KEY = os.environment['CRFR_SECRET_KEY']
-    DEBUG = os.environment['CRFR_DEBUG']
-    DB_NAME = os.environ['CRFR_DB_NAME']
-    DB_USER = os.environ['CRFR_DB_USER']
-    DB_PASS = os.environ['CRFR_DB_PASS']
-    DB_SERVICE = os.environ['CRFR_DB_SERVICE']
-    DB_PORT = os.environ['CRFR_DB_PORT']
+    SECRET_KEY = CRFR['SECRET_KEY']
+    DEBUG = CRFR['DEBUG']
+    DB_NAME = CRFR['DB_NAME']
+    DB_USER = CRFR['DB_USER']
+    DB_PASS = CRFR['DB_PASS']
+    DB_SERVICE = CRFR['DB_SERVICE']
+    DB_PORT = CRFR['DB_PORT']
     SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
         DB_USER, DB_PASS, DB_SERVICE, DB_PORT, DB_NAME
     )
